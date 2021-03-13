b228e236c7c4ccda6c9d5b0a2a1951fa14994d0f
MariaTimoshinina <76400935+MariaTimoshinina@users.noreply.github.com>
2021-03-13 19:26:27 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '1606075478:AAGy5jYTvkeFHVmJhhm2MKqGnGaxITn7RIc'
+
+keys = {
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+
+}
